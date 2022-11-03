cfca4086f00e891fbe70af75576ee0cfc02e6ed9
anowarzz <anowarhosen444@gmail.com>
2022-11-03 11:48:54 +0600

    basic mongo setup done

--- index.js
+++ index.js
@@ -3,10 +3,37 @@ const cors = require('cors');
 const app = express();
 const port =  process.env.PORT || 5000;
 var colors = require('colors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1rp4a3.mongodb.net/?retryWrites=true&w=majority`;
+
+
+ 
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
